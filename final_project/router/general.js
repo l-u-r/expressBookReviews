d16d9f787577b7f4d55eba6c1d8ce80e9f7f2f54
l-u-r <lourdescf@gmail.com>
2024-09-21 16:38:51 -0400

    tasks 10 to 14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,7 +24,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
@@ -73,5 +73,81 @@ public_users.get('/review/:isbn',function (req, res) {
     const ISBN = req.params.isbn;
     res.send(books[ISBN].reviews)
 });
+
+// Get the book list available in the shop using Promise callbacks or Async-Await
+function getBookList(){
+    return new Promise((resolve, reject) => {
+      resolve(books);
+    })
+}
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+    getBookList().then(
+      (b) => res.send(JSON.stringify(b, null, 4)),
+    );  
+});
+
+// Get book details based on ISBN using Promise callbacks or Async-Await
+function getBasedOnISBN(isbn){
+    let b = books[isbn];  
+    return new Promise((resolve, reject) => {
+        if (b) {
+            resolve(b);
+        }else{
+            reject("Can't find the book.");
+        }    
+    })
+}
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    getBasedOnISBN(isbn).then(
+        (b) => res.send(JSON.stringify(b, null, 4)),
+        (error) => res.send(error)
+    )
+});
+  
+// Get book details based on author using Promise callbacks or Async-Await
+function getBasedOnAuthor(author){
+    let list = [];
+    return new Promise((resolve, reject) => {
+        for (var isbn in books) {
+            let b = books[isbn];
+            if (b.author === author){
+                list.push(b);
+            }
+        }
+        resolve(list);  
+    })
+}
+// Get book details based on author
+public_users.get('/author/:author',function (req, res) {
+    const author = req.params.author;
+    getBasedOnAuthor(author).then(
+        result => res.send(JSON.stringify(result, null, 4))
+    );
+});
+  
+// Get all books based on title using Promise callbacks or Async-Await
+function getBasedOnTitle(title){
+    let list = [];
+    return new Promise((resolve, reject) => {
+        for (var isbn in books) {
+            let b = books[isbn];
+            if (b.title === title){
+                list.push(b);
+            }
+        }
+        resolve(list);  
+    })
+}
+// Get all books based on title
+public_users.get('/title/:title',function (req, res) {
+    const title = req.params.title;
+    getBasedOnTitle(title).then(
+        result => res.send(JSON.stringify(result, null, 4))
+    );
+});
+
 
 module.exports.general = public_users;
